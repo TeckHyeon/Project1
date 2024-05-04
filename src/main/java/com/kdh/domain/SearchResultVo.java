bd90ec9af8124b60a55170d4d40828ed02ae5724
TeckHyeon <rims21@naver.com>
2024-05-04 23:07:42 +0900

    검색결과 쿼리문 완성

--- src/main/java/com/kdh/domain/SearchResultVo.java
+++ src/main/java/com/kdh/domain/SearchResultVo.java
@@ -8,19 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchResultVo {
-	private Long user_idx;
-	private String user_id;
-	private String user_pw;
-	private String user_name;
-	private String user_email;
-	private String user_phone;
-	private String user_created_date;
-	private String user_updated_date;
-	private String deleted_yn;
-	private String user_comments;
-	private Long post_tag_idx;
-	private Long tag_idx;
-	private Long post_idx;
-	private ProfileVo profile = new ProfileVo();
-    
+	private String name;
+	private String description;
+	private Long count;
+	private String type;
 }
